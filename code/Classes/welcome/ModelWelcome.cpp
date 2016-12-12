7a263c87ba5e8dafec227b5b9018bd77fd9cbb52
马诤 <mzzz.123@foxmail.com>
2016-12-12 17:02:42 +0800

    V0.0.0017

--- code/Classes/welcome/ModelWelcome.cpp
+++ code/Classes/welcome/ModelWelcome.cpp
@@ -1,12 +1,17 @@
 #include "ModelWelcome.h"
+#include "logo/LayerLogo.h"
 
-ModelWelcome::ModelWelcome() : _listener(nullptr)
+USING_NS_CC;
+
+ModelWelcome::ModelWelcome() : 
+	_listenerLayerLogoOver(nullptr), 
+	_state(StateWelcome::UNPLAY_LOGO)
 {
 }
 
 ModelWelcome::~ModelWelcome()
 {
-	_listener = nullptr;
+	_listenerLayerLogoOver = nullptr;
 }
 
 bool ModelWelcome::init()
@@ -19,4 +24,59 @@ bool ModelWelcome::init()
 	} while (0);
 
 	return isInit;
+}
+
+Layer* ModelWelcome::getLayer(const TypeLayerInWelcome& type)
+{
+	auto layer = getLayerByType(type);
+	if (layer == nullptr)
+	{
+		layer = createLayerByType(type);
+		insertLayerByType(type, layer);
+	}
+	return layer;
+}
+
+void ModelWelcome::setLayerNullptr(const TypeLayerInWelcome& type)
+{
+	eraseLayerByType(type);
+}
+
+bool ModelWelcome::insertLayerByType(const TypeLayerInWelcome& type, Layer* layer)
+{
+	auto layerInDic = getLayerByType(type);
+	if (layerInDic != nullptr)
+	{
+		return false;
+	}
+	_dicLayers.insert(type, layer);
+	return true;
+}
+
+Layer* ModelWelcome::createLayerByType(const TypeLayerInWelcome& type)
+{
+	switch (type)
+	{
+	case TypeLayerInWelcome::LOGO:
+		return LayerLogo::create();
+	default:
+		return nullptr;
+	}
+}
+
+bool ModelWelcome::eraseLayerByType(const TypeLayerInWelcome& type)
+{
+	auto layer = getLayerByType(type);
+	if (layer == nullptr)
+	{
+		return false;
+	}
+	_dicLayers.erase(type);
+	return true;
+}
+
+Layer* ModelWelcome::getLayerByType(const TypeLayerInWelcome& type)
+{
+	auto layer = _dicLayers.at(type);
+	return layer;
 }
